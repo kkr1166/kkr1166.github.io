0e0cf1a69f6f750d69ae42685f670fc40f44d573
Kitra <kitrarazin2020@u.northwestern.edu>
2020-06-30 16:16:10 -0500

    rerouting

--- project/js/acoustic.js
+++
@@ -1,9 +0,0 @@
-//function seeInfo() {
-//    var x = document.querySelector('.flip-card-inner');
-//    x.style.transform = "rotateY(-180deg)";
-//}
-//
-//function seePoster() {
-//    var x = document.querySelector('.flip-card-inner');
-//    x.style.transform = "none";
-//}
